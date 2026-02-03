8b02e5d3760f74ceee00dc54ec05a8e3411512ee
Jorge Alberto Norzagaray Mora <jorge.norzagaray16329@potros.itson.edu.mx>
2026-02-02 23:22:36 -0700

    Iniciando proyecto GIT

---
+++ src/main/java/mx/edu/itson/ejerciciogit01/EjercicioGIT01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itson.ejerciciogit01;
+
+/**
+ *
+ * @author jorge
+ */
+public class EjercicioGIT01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
